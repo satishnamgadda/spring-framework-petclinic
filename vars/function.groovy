8c5e3ff2273ce68bbe0b060210ecd708d2c75437
ns <namgaddasatish@gmail.com>
2022-10-23 09:35:03 +0530

    added chnges

--- vars/function.groovy
+++ vars/function.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('vcs') {
             steps {
-                git branch: "$REL_INT_1.0", url: 'https://github.com/satishnamgadda/spring-framework-petclinic.git'
+                git branch: "REL_INT_1.0", url: 'https://github.com/satishnamgadda/spring-framework-petclinic.git'
             }
 
         }
